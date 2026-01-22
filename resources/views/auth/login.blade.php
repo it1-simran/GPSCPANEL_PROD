96d13faae4c051c7b9a4822c529d89dcade0df18
it1-simran <it1@jsdelectronics.co.in>
2026-01-22 17:51:31 +0530

    test workflow

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ isset($url) ? ucwords($url) : ""}} {{ __('Login') }}</div>
+                <div class="card-header">{{ isset($url) ? ucwords($url) : ""}} {{ __('Login') }} Test Workflow</div>
 
                 <div class="card-body">
                     @isset($url)

e62b230504c2890157094a3fae7c4b4e830591ac
Gurpreet <it2@jsdelectronics.co.in>
2026-04-11 18:24:03 +0530

    working on live racking filters

--- simulate_traffic.php
+++ simulate_traffic.php
@@ -14,7 +14,7 @@
 // Configuration
 $baseUrl = "http://localhost:8000";
 $apiIngestUrl = "$baseUrl/api/packets/ingest";
-$imei = "123456789012345";
+$imei = isset($argv[1]) ? $argv[1] : "123456789012345";
 $trafficIterations = 15;
 $trafficDelay = 3; // Seconds between traffic packets
 
